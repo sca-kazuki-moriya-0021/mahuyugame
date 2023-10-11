c3acd3b62dfcac1cfffc7acb1a746f6c12670dcd
moriya <21cge0021mk@edu.sca.ac.jp>
2023-10-11 13:35:32 +0900

    GM生成

---
+++ Assets/Scr/TotalGM.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class TotalGM : MonoBehaviour
+{
+    #region//ステージ管理
+
+    //ステージ管理
+    public enum StageCon
+    {
+        Title = 0,
+        Fiast,
+        GameOver,
+        Clear,
+    }
+
+    //現在・前・クリア時に戻る時に使う変数
+    private StageCon scene;
+    private StageCon backScene;
+    private StageCon clearBackScene;
+
+    private Dictionary<string, StageCon> sceneDic = new Dictionary<string, StageCon>()
+    {
+        {"Title",StageCon.Title },
+        {"Stage",StageCon.Fiast },
+        {"GameOver",StageCon.GameOver },
+        {"Clear",StageCon.Clear },
+    };
+
+    #endregion
+
+    public StageCon Scene {
+        get { return this.scene; }
+    }
+
+
+    public StageCon BackScene {
+        get { return this.backScene; }
+        set { this.backScene = value; }
+    }
+
+    public StageCon ClearBackScene {
+        get { return this.clearBackScene; }
+        set { this.clearBackScene = value; }
+    }
+
+    public Dictionary<string, StageCon> SceneDic {
+        get { return this.sceneDic; }
+        set { this.sceneDic = value; }
+    }
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //現在のステージを返す
+    public StageCon MyGetScene()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        scene = sceneDic[sceneName];
+        return scene;
+    }
+
+    // enumのシーンで指定したシーンをロードする
+    public void MyLoadScene(StageCon scene)
+    {
+        SceneManager.LoadScene(sceneDic.FirstOrDefault(x => x.Value == scene).Key);
+    }
+
+    // 現在のシーンを再度ロードする
+    public void ReloadCurrentScene()
+    {
+        StageCon scene = MyGetScene();
+        MyLoadScene(scene);
+    }
+
+    //前のシーンに戻る時に使う
+    public void ReloadClearScene()
+    {
+        MyLoadScene(backScene);
+    }
+
+    //クリアからステージに戻る時に使う
+    public void ClearBack()
+    {
+        MyLoadScene(clearBackScene);
+    }
+}
