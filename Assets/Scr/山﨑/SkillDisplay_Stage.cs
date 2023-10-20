29d087516ac65319d467d13348df10522960dd5b
Yamazaki <21cge0022yr@edu.sca.ac.jp>
2023-10-20 18:51:54 +0900

    スキル

--- Assets/Scr/山﨑/SkillDisplay_Stage.cs
+++ Assets/Scr/山﨑/SkillDisplay_Stage.cs
@@ -7,21 +7,29 @@ public class SkillDisplay_Stage : MonoBehaviour
 {
     [SerializeField] private Sprite[] skillicon;
     [SerializeField] private Image[] skillPosition;
-    //[SerializeField] private Image secondSkill;
+    [SerializeField] private Image[] ui;
+    private bool[] skillCoolFlag = new bool[] {false ,false};
+    [SerializeField] private float[] coolTime = new float[] {0,0,0,0};
+    private Player player;
     private TotalGM totalGM;
     int count;
     //private bool[] playerSkill = new bool[] { false, false, false, false };
 
+    public bool[] SkillCoolTime {
+        get { return this.skillCoolFlag; }
+        set { this.skillCoolFlag = value; }
+    }
+
     // Start is called before the first frame update
+
+    //ƒXƒLƒ‹‚Ì•\Ž¦
     void Start()
     {
         totalGM = FindObjectOfType<TotalGM>();
+        player = FindObjectOfType<Player>();
         for ( int count = 0; count <= 3; count++)
         {
-            /*
-            if (count >= 3) { 
-            count=3; }
-            */
+
             if (totalGM.PlayerSkill[count] == true)
             {
                 if (skillPosition[0].sprite == null)
@@ -30,6 +38,36 @@ public class SkillDisplay_Stage : MonoBehaviour
                     skillPosition[1].sprite = skillicon[count];
             }
         }
-       
+
+    }
+
+    //coolTime
+    private void Update()
+    {
+        if(/*player.SkillAtkFlag[0] &&*/ !skillCoolFlag[0])
+        {
+            skillCoolFlag[0] = false;
+            ui[0].fillAmount -= 1.0f / coolTime[0] * Time.deltaTime;
+            if (ui[0].fillAmount <= 0)
+            {
+
+                skillCoolFlag[0] = true;
+                ui[0].fillAmount = 1;
+                coolTime[0] = 0;
+            }
+        }
+
+        if (/*player.SkillAtkFlag[1] && */ !skillCoolFlag[1])
+        {
+            skillCoolFlag[1] = false;
+            ui[1].fillAmount -= 1.0f / coolTime[1] * Time.deltaTime;
+            if(ui[1].fillAmount <= 0)
+            {
+
+                skillCoolFlag[1] = true;
+                ui[1].fillAmount = 1;
+                coolTime[1] = 0;
+            }
+        }
     }
 }
