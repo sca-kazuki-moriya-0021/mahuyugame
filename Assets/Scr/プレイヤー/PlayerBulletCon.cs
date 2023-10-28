39f371d972f4dc8ec2ff6f4e4aae60c340baf305
moriya <21cge0021mk@edu.sca.ac.jp>
2023-10-28 18:39:39 +0900

    ついか

---
+++ Assets/Scr/プレイヤー/PlayerBulletCon.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PlayerBulletPool;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+
+public class PlayerBulletCon : MonoBehaviour
+{
+    //弾保存用
+    [SerializeField]
+    private GameObject[] bullets;
+    //弾の発射位置
+    [SerializeField]
+    private GameObject[] bulletsPosition;
+
+    [SerializeField]
+    private float velocity;
+    [SerializeField]
+    private float degree;
+    [SerializeField]
+    private float angle_split;
+
+    private float theta;
+    float PI = Mathf.PI;
+
+    private float time;
+
+    private TotalGM gm;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<TotalGM>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > 5)
+        {
+            if (gm.PlayerWeapon[0] == true)
+            {
+                for (int i = 0; i < bulletsPosition.Length; i++)
+                {
+                    Instantiate(bullets[0], bulletsPosition[i].transform);
+                }
+            }
+
+            if (gm.PlayerWeapon[1] == true)
+            {
+                for (int i = 0; i <= (angle_split - 1); i++)
+                {
+                    //n-way弾の端から端までの角度
+                    float AngleRange = PI * (degree / 180);
+
+                    //弾インスタンスに渡す角度の計算
+                    if (angle_split > 1) theta = (AngleRange / (angle_split - 1)) * i - 0.5f * AngleRange;
+                    else theta = 0;
+
+                    //弾インスタンスを取得し、初速と発射角度を与える
+                    GameObject bullet_obj = (GameObject)Instantiate(bullets[1], transform.position, transform.rotation);
+                    LaserBullet bullet_sc = bullet_obj.GetComponent<LaserBullet>();
+                    bullet_sc.Theta = theta;
+                    bullet_sc.Velocity = velocity;
+                }
+
+            }
+            time = 0;
+        }
+       
+    }
+}
