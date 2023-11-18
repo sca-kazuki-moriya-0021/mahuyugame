59dc56cbf6920190b34467eb8122aabea8f4cbf5
syuugo <21cge0002is@edu.sca.ac.jp>
2023-11-18 16:39:58 +0900

    エネミーのスキルカットイン完成

---
+++ Assets/Scr/伊藤/EnemySkillCutIn.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemySkillCutIn : MonoBehaviour
+{
+    [SerializeField,Header("エネミーのスキルカットインイラスト")]
+    private Sprite[] EnemyskillSprites;
+
+    [SerializeField,Header("取得する画像")]
+    private Image getImage;
+
+    [SerializeField,Header("カットイン用の文章")]
+    private string[] Enemytexts;
+
+    [SerializeField,Header("取得する文章")]
+    private Text getEnemyText;
+
+    [SerializeField,Header("エネミーのスキルカットインのアニメーション")]
+    private Animator[] EnemyskillAnimator;
+
+    private Canvas EnemyCanvas;
+
+    private bool cutInFlagEnemy;
+
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyCanvas=this.GetComponent<Canvas>();
+
+        EnemyCanvas.enabled=false;
+
+        EnemyCutInDisplay(6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cutInFlagEnemy == true)
+        {
+            time+=Time.unscaledDeltaTime;
+            if (time >= 3f)
+            {
+                time=0;
+                Time.timeScale=1f;
+                EnemyCanvas.enabled=false;
+            }
+        }
+    }
+
+    public void EnemyCutInDisplay(int i)
+    {
+        for(int x = 0; x < EnemyskillAnimator.Length; x++)
+        {
+            Debug.Log("スキル発動");
+            EnemyskillAnimator[x].updateMode=AnimatorUpdateMode.UnscaledTime;
+        }
+        EnemyCanvas.enabled=true;
+        time=0;
+        Time.timeScale=0f;
+        getImage.sprite=EnemyskillSprites[i];
+        getEnemyText.text=Enemytexts[i];
+        cutInFlagEnemy= true;
+    }
+}
