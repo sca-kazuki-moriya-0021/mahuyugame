8a42d578f08f683441c48b75f7b42320084834b1
Yama <21cge0022yr@edu.sca.ac.jp>
2023-11-18 15:24:09 +0900

    NowLoading完成？

---
+++ Assets/Scr/山﨑/NowLoading.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NowLoading : MonoBehaviour
+{
+    [SerializeField]private Image backGround;
+    private TotalGM totalGM;
+    // Start is called before the first frame update
+
+
+
+    void Start()
+    {
+        //”O‚Ì‚½‚ß
+        backGround.enabled = false;
+        totalGM = FindObjectOfType<TotalGM>();
+        //Loading();
+        FadeOut();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void  FadeIn()
+    {
+        backGround.enabled=true;
+        backGround.DOFade(2.55f,1.0f).SetEase(Ease.Linear).SetDelay(1.0f).OnComplete(() =>
+        { 
+            var scene = totalGM.MyGetScene();
+            switch (scene)
+            {
+                case TotalGM.StageCon.First:
+                    SceneManager.LoadScene("SecondStage");
+                    break;
+                case TotalGM.StageCon.Secound:
+                    SceneManager.LoadScene("TheadStage");
+                    break;
+                case TotalGM.StageCon.Thead:
+                    SceneManager.LoadScene("Clear");
+                    break;
+            }
+            
+        });
+    }
+    private void FadeOut()
+    {
+        backGround.DOFade(0f, 2.0f).SetEase(Ease.Linear);
+    }
+    
+}
