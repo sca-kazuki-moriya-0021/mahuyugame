680c343e97d809a2ecea24bf75c7dd17fe8d67e7
moriya <21cge0021mk@edu.sca.ac.jp>
2023-11-18 11:53:03 +0900

    エリアマネージャー見直し

--- Assets/Scr/マネージャー関係/AreaManager.cs
+++ Assets/Scr/マネージャー関係/AreaManager.cs
@@ -6,21 +6,42 @@ public class AreaManager : MonoBehaviour
 {
     private bool bossActiveFlag;
 
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip[] audioClips;
+
+    private bool bossPhaseFlag = false;
+
+
     public bool BossActiveFlag
     {
         get { return this.bossActiveFlag; }
         set { this.bossActiveFlag = value; }
     }
 
+    public bool BossPhaseFlag
+    {
+        get { return this.bossPhaseFlag; }
+        set { this.bossPhaseFlag = value; }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioClips[0];
+        audioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (bossPhaseFlag == true)
+        {
+            audioSource.clip = audioClips[1];
+            audioSource.Play();
+            bossPhaseFlag = false;
+        }
     }
 }
