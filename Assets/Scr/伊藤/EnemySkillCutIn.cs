6091129015389d4d0a2fc3a17d1562ac5d839af9
syugo <21cge0002is@edu.sca.ac.jp>
2023-11-25 18:48:15 +0900

    no message

--- Assets/Scr/伊藤/EnemySkillCutIn.cs
+++ Assets/Scr/伊藤/EnemySkillCutIn.cs
@@ -6,21 +6,21 @@ using UnityEngine.UI;
 public class EnemySkillCutIn : MonoBehaviour
 {
     [SerializeField,Header("エネミーのスキルカットインイラスト")]
-    private Sprite[] EnemyskillSprites;
+    private Sprite[] enemyskillSprites;
 
     [SerializeField,Header("取得する画像")]
     private Image getImage;
 
     [SerializeField,Header("カットイン用の文章")]
-    private string[] Enemytexts;
+    private string[] enemytexts;
 
     [SerializeField,Header("取得する文章")]
-    private Text getEnemyText;
+    private Text getenemyText;
 
     [SerializeField,Header("エネミーのスキルカットインのアニメーション")]
-    private Animator[] EnemyskillAnimator;
+    private Animator[] enemyskillAnimator;
 
-    private Canvas EnemyCanvas;
+    private Canvas enemyCanvas;
 
     private bool cutInFlagEnemy;
 
@@ -28,9 +28,9 @@ public class EnemySkillCutIn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        EnemyCanvas=this.GetComponent<Canvas>();
+        enemyCanvas=this.GetComponent<Canvas>();
 
-        EnemyCanvas.enabled=false;
+        enemyCanvas.enabled=false;
 
         EnemyCutInDisplay(6);
     }
@@ -45,22 +45,22 @@ public class EnemySkillCutIn : MonoBehaviour
             {
                 time=0;
                 Time.timeScale=1f;
-                EnemyCanvas.enabled=false;
+                enemyCanvas.enabled=false;
             }
         }
     }
 
     public void EnemyCutInDisplay(int i)
     {
-        for(int x = 0; x < EnemyskillAnimator.Length; x++)
+        for(int x = 0; x < enemyskillAnimator.Length; x++)
         {
-            EnemyskillAnimator[x].Rebind();
-            EnemyskillAnimator[x].updateMode=AnimatorUpdateMode.UnscaledTime;
+            enemyskillAnimator[x].Rebind();
+            enemyskillAnimator[x].updateMode=AnimatorUpdateMode.UnscaledTime;
         }
-        EnemyCanvas.enabled=true;
+        enemyCanvas.enabled=true;
         Time.timeScale=0f;
-        getImage.sprite=EnemyskillSprites[i];
-        getEnemyText.text=Enemytexts[i];
+        getImage.sprite=enemyskillSprites[i];
+        getenemyText.text=enemytexts[i];
         cutInFlagEnemy= true;
     }
 }
