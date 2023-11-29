e13006e4d9121a11320cca562bcc7b447264ad2e
moriya <21cge0021mk@edu.sca.ac.jp>
2023-11-29 14:49:48 +0900

    メイン・サブ弾幕完成

--- Assets/Scr/プレイヤー/PlayerBulletCon.cs
+++ Assets/Scr/プレイヤー/PlayerBulletCon.cs
@@ -12,14 +12,23 @@ public class PlayerBulletCon : MonoBehaviour
     [SerializeField]
     private GameObject skillBullet;
 
-    //弾の発射位置
-    private GameObject[] bulletChilds = new GameObject[]{null,null,null};
+    //メイン弾の発射位置
+    [SerializeField,Header("メイン弾発射位置")]
+    private GameObject mainBulletPos;
+    private GameObject[] mainBulletPosChird = new GameObject[3] {null,null,null};
+    //サブ弾の発射位置
+    [SerializeField, Header("サブ弾発射位置")]
+    private GameObject subBulletPos;
+    private GameObject[] subBulletPosChird = new GameObject[2]{ null,null};
+
 
     //レーザー弾設定用変数
     [SerializeField, Header("レーザーの速度")]
     private float laserVelocity;
     [SerializeField,Header("レーザーの発射角度")]
     private float[] laserAngle;
+    [SerializeField, Header("サブレーザーの発射角度")]
+    private float[] subLaserAngle;
 
     //ブーメラン弾設定用変数
     [SerializeField,Header("ブーメランの速度")]
@@ -47,20 +56,30 @@ public class PlayerBulletCon : MonoBehaviour
         player = FindObjectOfType<Player>();
         playerCollider = FindObjectOfType<PlayerCollider>();
 
-        //自分の子オブジェクトを取得
-        int childCount = this.gameObject.transform.childCount;
-        for(int i = 0; i < childCount-1; i++)
+        //子オブジェクトを取得
+        for(int i = 0; i < 3; i++)
         {
-           Transform childTransform = this.gameObject.transform.GetChild(i);
-           bulletChilds[i] = childTransform.gameObject;
+            mainBulletPosChird[i] = mainBulletPos.transform.GetChild(i).gameObject;
         }
-
+          
+        for(int i = 0; i < 2; i++)
+        {
+            subBulletPosChird[i] = subBulletPos.transform.GetChild(i).gameObject;
+        }
+        
         //角度をラジアンに変換
         for(int i = 0; i < laserAngle.Length; i++)
         {
             laserAngle[i] = laserAngle[i] * Mathf.Deg2Rad;
         }
 
+        for(int i = 0; i< subLaserAngle.Length; i++)
+        {
+            subLaserAngle[i] = subLaserAngle[i] * Mathf.Deg2Rad;
+        }
+
+        gm.PlayerWeapon[0] = true;
+        gm.PlayerSubWeapon[1] = true;
     }
 
     // Update is called once per frame
@@ -89,53 +108,59 @@ public class PlayerBulletCon : MonoBehaviour
         }
     }
 
+    //メインの発射位置
     private void MainBullet()
     {
         //選択された0番目の弾を発射する
         if (gm.PlayerWeapon[0] == true)
-           Weapon0();
+           Weapon0(mainBulletPosChird);
         //選択された1番目の弾を発射する
         else if (gm.PlayerWeapon[1] == true)
-           Weapon1();
+           Weapon1(mainBulletPosChird);
         //選択された2番目の弾を発射する
         else if (gm.PlayerWeapon[2] == true)
-           Weapon2();
+           Weapon2(mainBulletPosChird);
         //選択された3番目の弾を発射する
         else if (gm.PlayerWeapon[3] == true)
-           Weapon3();
+           Weapon3(mainBulletPosChird);
     }
-
+    //サブの発射位置
     private void SubBullet()
     {
         if (gm.PlayerSubWeapon[0] == true)
-            Weapon0();
+            Weapon0(subBulletPosChird);
         //選択された1番目の弾を発射する
         else if (gm.PlayerSubWeapon[1] == true)
-            Weapon1();
+            Weapon1(subBulletPosChird);
         //選択された2番目の弾を発射する
         else if (gm.PlayerSubWeapon[2] == true)
-            Weapon2();
+            Weapon2(subBulletPosChird);
         //選択された3番目の弾を発射する
         else if (gm.PlayerSubWeapon[3] == true)
-            Weapon3();
+            Weapon3(subBulletPosChird);
     }
 
-    private void Weapon0()
+    private void Weapon0(GameObject[]bulletPos)
     {
-        for (int i = 0; i < bulletChilds.Length; i++)
-        {
-            Instantiate(bullets[0], bulletChilds[i].transform.position, Quaternion.identity);
-        }
+        for (int i = 0; i < bulletPos.Length; i++)
+           Instantiate(bullets[0],bulletPos[i].transform.position, Quaternion.identity);
     }
 
-    private void Weapon1()
+    private void Weapon1(GameObject[] bulletPos)
     {
-        for (int i = 0; i < laserAngle.Length; i++)
+        GameObject[] weapen = bulletPos;
+        float[] angle;
+        if(weapen == mainBulletPosChird)
+            angle = laserAngle;
+        else
+            angle = subLaserAngle;
+
+        for (int i = 0; i < angle.Length; i++)
         {
-            Vector3 dir = new Vector2(Mathf.Cos(laserAngle[i]), Mathf.Sin(laserAngle[i]));
+            Vector3 dir = new Vector2(Mathf.Cos(angle[i]), Mathf.Sin(angle[i]));
             dir.z = 0;
             //弾インスタンスを取得し、初速と発射角度を与える
-            GameObject bullet_obj = (GameObject)Instantiate(bullets[1], bulletChilds[i].transform.position, transform.rotation);
+            GameObject bullet_obj = (GameObject)Instantiate(bullets[1], bulletPos[i].transform.position, transform.rotation);
             LaserBullet bullet_sc = bullet_obj.GetComponent<LaserBullet>();
             if (player.PBaffSkillFlag == true)
                 bullet_sc.Velocity = laserVelocity * 1.5f;
@@ -145,21 +170,19 @@ public class PlayerBulletCon : MonoBehaviour
         }
     }
 
-    private void Weapon2()
+    private void Weapon2(GameObject[] bulletPos)
     {
-        for (int i = 0; i < bulletChilds.Length; i++)
-        {
-            Instantiate(bullets[2], bulletChilds[i].transform.position, Quaternion.identity);
-        }
+        for (int i = 0; i < bulletPos.Length; i++)
+            Instantiate(bullets[2], bulletPos[i].transform.position, Quaternion.identity);
     }
 
-    private void Weapon3()
+    private void Weapon3(GameObject[] bulletPos)
     {
-        for (int i = 0; i < bulletChilds.Length; i++)
+        for (int i = 0; i < bulletPos.Length; i++)
         {
             //弾インスタンスを取得し、初速と発射角度を与える
-            var dir = boomerangPoint.transform.position - bulletChilds[i].transform.position / 2;
-            GameObject bullet_obj = (GameObject)Instantiate(bullets[3], bulletChilds[i].transform.position, transform.rotation);
+            var dir = boomerangPoint.transform.position - bulletPos[i].transform.position / 2;
+            GameObject bullet_obj = (GameObject)Instantiate(bullets[3], bulletPos[i].transform.position, transform.rotation);
             BoomerangBullet bullet_sc = bullet_obj.GetComponent<BoomerangBullet>();
             if (player.PBaffSkillFlag == true)
                 bullet_sc.Velocity = boomerangVelocity * 1.5f;
