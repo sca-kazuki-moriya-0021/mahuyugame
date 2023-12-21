3bbd4560190438dd9aa7dcdcf11753d1fb0b490d
moriya <21cge0021mk@edu.sca.ac.jp>
2023-12-21 14:07:59 +0900

    ゆきおんなの弾修正

--- Assets/Scr/ボス/ゆきおんな/弾/SnowCrystalBullet.cs
+++ Assets/Scr/ボス/ゆきおんな/弾/SnowCrystalBullet.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class SnowCrystalBullet : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private float bulletSpeed;
+    [SerializeField]
+    private int numberOfShots;
+    [SerializeField]
+    private int numberOfBullets;
+    [SerializeField]
+    private float spreadAngle;
+    [SerializeField]
+    private float fireTime;
+
     private Player player;
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<Player>();
+        StartCoroutine(ShootMultiSpread());
     }
 
     // Update is called once per frame
@@ -20,6 +34,34 @@ public class SnowCrystalBullet : MonoBehaviour
         }
     }
 
+
+    IEnumerator ShootMultiSpread()
+    {
+        while (true)
+        {
+            for (int i = 0; i < numberOfShots; i++)
+            {
+                ShootBullets();
+                yield return new WaitForSeconds(0.2f);
+            }
+            yield return new WaitForSeconds(fireTime);
+        }
+    }
+
+    private void ShootBullets()
+    {
+        float startAngle = -spreadAngle / 2;
+
+        for (int i = 0; i < numberOfBullets; i++)
+        {
+            float angle = startAngle + i * (spreadAngle / (numberOfBullets));
+
+            // ’e‚ð”­ŽË
+            Vector3 direction = Quaternion.Euler(0, 0, angle) * Vector3.up;
+            Rigidbody2D bulletRigidbody = Instantiate(bulletPrefab, transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+            bulletRigidbody.velocity = direction * bulletSpeed;
+        }
+    }
 
     private void OnBecameInvisible()
     {
---
+++ Assets/Scr/ボス/ゆきおんな/弾/SnowCrystalChildBullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowCrystalChildBullet : MonoBehaviour
+{
+    private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.BulletSeverFlag == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
