05512f984844f06f91089fbe414630cf7c59e055
moriya <21cge0021mk@edu.sca.ac.jp>
2023-12-22 16:50:01 +0900

    sukiru

--- Assets/Scr/山﨑/BarrageSelect.cs
+++ Assets/Scr/山﨑/BarrageSelect.cs
@@ -21,7 +21,7 @@ public class BarrageSelect : MonoBehaviour
     [SerializeField] float outLineSizeB_X;
     [SerializeField] float outLineSizeB_Y;
     [SerializeField] GameObject goStageButton;
-    bool[] mainSubWepon = {false,false};
+    bool[] mainSubWepon = { false, false };
 
     private void Awake()
     {
@@ -44,7 +44,8 @@ public class BarrageSelect : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log(mainSubWepon[0]);
+        Debug.Log(barrageCount);
+        Debug.Log(totalGM.PlayerSubWeapon[3]);
         if (selectedObj == null)
         {
             button.Select();
@@ -54,35 +55,42 @@ public class BarrageSelect : MonoBehaviour
         {
             selectedObj = ev.currentSelectedGameObject;
             outLine.transform.position = selectedObj.transform.position;
-            //OutLineSize();
+            OutLineSize();
         }
     }
 
     //押されたときの処理
     public void Skill_0_Click()
     {
-        if(!mainSubWepon[0])
+        if (!mainSubWepon[0])
         {
-            mainSubWepon[0] =true;
+            mainSubWepon[0] = true;
             barrageCount++;
             totalGM.PlayerWeapon[0] = true;
             barrageSelect[0].enabled = true;
         }
-        else if(mainSubWepon[0] && !mainSubWepon[1])
+        else if (mainSubWepon[0] && !mainSubWepon[1] && !totalGM.PlayerWeapon[0] && barrageCount == 1)
         {
-            mainSubWepon[1]=true;
+            mainSubWepon[1] = true;
             barrageCount++;
             totalGM.PlayerSubWeapon[0] = true;
             barrageSelect[0].enabled = true;
         }
-        else if(barrageCount==2 && mainSubWepon[0])
+        else if (totalGM.PlayerWeapon[0])
         {
+            //Debug.Log("AA");
             mainSubWepon[0] = false;
-            mainSubWepon[1] = false;
-            barrageCount--;
             barrageSelect[0].enabled = false;
             totalGM.PlayerWeapon[0] = false;
+            barrageCount--;
+        }
+        else if (totalGM.PlayerSubWeapon[0])
+        {
+            mainSubWepon[1] = false;
+            barrageSelect[0].enabled = false;
             totalGM.PlayerSubWeapon[0] = false;
+            barrageCount--;
+
         }
         TwoSelect();
     }
@@ -96,25 +104,31 @@ public class BarrageSelect : MonoBehaviour
             totalGM.PlayerWeapon[1] = true;
             barrageSelect[1].enabled = true;
         }
-        else if (mainSubWepon[0] && !mainSubWepon[1])
+        else if (mainSubWepon[0] && !mainSubWepon[1] && !totalGM.PlayerWeapon[1] && barrageCount == 1)
         {
-            
+
             barrageCount++;
             totalGM.PlayerSubWeapon[1] = true;
             barrageSelect[1].enabled = true;
             mainSubWepon[1] = true;
-            Debug.Log("A");
+            //Debug.Log("A");
         }
-        else
+        else if (totalGM.PlayerWeapon[1])
         {
-            
             mainSubWepon[0] = false;
-            mainSubWepon[1] = false;
-            barrageCount--;
-            barrageSelect[1].enabled = false;
+            barrageSelect[0].enabled = false;
             totalGM.PlayerWeapon[1] = false;
+            barrageCount--;
+
+
+        }
+        else if (totalGM.PlayerSubWeapon[1])
+        {
+            mainSubWepon[1] = false;
+            barrageSelect[0].enabled = false;
             totalGM.PlayerSubWeapon[1] = false;
-            Debug.Log("AA");
+            barrageCount--;
+
         }
         TwoSelect();
     }
@@ -128,21 +142,28 @@ public class BarrageSelect : MonoBehaviour
             totalGM.PlayerWeapon[2] = true;
             barrageSelect[2].enabled = true;
         }
-        else if (mainSubWepon[0] && !mainSubWepon[1])
+        else if (mainSubWepon[0] && !mainSubWepon[1] && !totalGM.PlayerWeapon[2] && barrageCount == 1)
         {
             mainSubWepon[1] = true;
             barrageCount++;
             totalGM.PlayerSubWeapon[2] = true;
             barrageSelect[2].enabled = true;
         }
-        else
+        else if (totalGM.PlayerWeapon[2])
         {
             mainSubWepon[0] = false;
-            mainSubWepon[1] = false;
-            barrageCount--;
             barrageSelect[2].enabled = false;
             totalGM.PlayerWeapon[2] = false;
+            barrageCount--;
+
+
+        }
+        else if (totalGM.PlayerSubWeapon[2])
+        {
+            mainSubWepon[1] = false;
+            barrageSelect[2].enabled = false;
             totalGM.PlayerSubWeapon[2] = false;
+            barrageCount--;
 
         }
         TwoSelect();
@@ -157,21 +178,45 @@ public class BarrageSelect : MonoBehaviour
             totalGM.PlayerWeapon[3] = true;
             barrageSelect[3].enabled = true;
         }
-        else if (mainSubWepon[0] && !mainSubWepon[1])
+        else if (mainSubWepon[0] && !mainSubWepon[1] && !totalGM.PlayerWeapon[3] && barrageCount == 1)
         {
             mainSubWepon[1] = true;
             barrageCount++;
             totalGM.PlayerSubWeapon[3] = true;
             barrageSelect[3].enabled = true;
         }
-        else
+        else if (totalGM.PlayerWeapon[3])
         {
-            barrageCount--;
+            mainSubWepon[0] = false;
             barrageSelect[3].enabled = false;
             totalGM.PlayerWeapon[3] = false;
+            barrageCount--;
+
+
+        }
+        else if (totalGM.PlayerSubWeapon[3])
+        {
+            mainSubWepon[1] = false;
+            barrageSelect[3].enabled = false;
             totalGM.PlayerSubWeapon[3] = false;
+            barrageCount--;
+
         }
         TwoSelect();
+    }
+    private void OutLineSize()
+    {
+        if (selectedObj.gameObject.CompareTag("Button"))
+        {
+            outLine.enabled = false;
+            outLine.transform.localScale = new Vector2(outLineSizeS_X, outLineSizeS_Y);
+        }
+        else
+        {
+            outLine.enabled = true;
+            outLine.transform.localScale = new Vector2(outLineSizeB_X, outLineSizeB_Y);
+
+        }
     }
     void TwoSelect()
     {
@@ -190,7 +235,7 @@ public class BarrageSelect : MonoBehaviour
 
             if (!totalGM.PlayerWeapon[i] && barrageCount == 1)
             {
-                //Debug.Log("スキル1ッコ");
+                
                 //barrage[i].enabled = false;//interactableにすると半透明化
                 barrageSelect[i].enabled = false;
             }
@@ -201,7 +246,7 @@ public class BarrageSelect : MonoBehaviour
 
             if (barrageCount == 0)
             {
-                barrageSelect[i].enabled= false;
+                barrageSelect[i].enabled = false;
                 barrage[i].enabled = true;
             }
         }
