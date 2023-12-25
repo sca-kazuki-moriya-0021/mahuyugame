9f4b5faf32412eb661a83d0fafa2fd9d5069fae3
moriya <21cge0021mk@edu.sca.ac.jp>
2023-12-25 17:48:56 +0900

    レイヤー分けと背景スクロール仮組み

--- Assets/Scr/森屋/TestBaukGround.cs
+++ Assets/Scr/森屋/TestBaukGround.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class TestBaukGround : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float ResetPosition;
+    private float MoveSpeed = -0.05f;
+    private Vector3 StartPosition;
+    void Awake()
     {
-        
+        StartPosition = transform.position;
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(MoveSpeed, 0, 0, Space.World);
+        if (transform.position.x < ResetPosition)
+            transform.position = StartPosition;
     }
 }
