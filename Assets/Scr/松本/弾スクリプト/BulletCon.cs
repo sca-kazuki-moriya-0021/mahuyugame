aa5f02244e4e80fe464960b1d0828bc6bf91f32f
matu <21cge0019mr@edu.sca.ac.jp>
2024-01-13 11:57:16 +0900

    反射弾追加

---
+++ Assets/Scr/松本/弾スクリプト/BulletCon.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCon : MonoBehaviour
+{
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncrementCount()
+    {
+        count++;
+    }
+
+    public int GetCount()
+    {
+        return count;
+    }
+}
