c548e7aea4cf89888feb558091200f98a93abd9d
sca-syuugo-itou0002 <21cge0002is@edu.sca.ac.jp>
2024-01-25 23:26:06 +0900

    クリア画面アニメーション表示部分仮完成

--- Assets/Scr/伊藤/TextAnimation.cs
+++ Assets/Scr/伊藤/TextAnimation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System;
 
 public class TextAnimation : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class TextAnimation : MonoBehaviour
     [SerializeField] private Text[] _text;
     [SerializeField] private float[] score;
     [SerializeField,Header("目標地点")] private Vector2[] posion; 
-    [SerializeField,Header("テキスト表示するまでの時間")] private float texttime;
+    [SerializeField,Header("テキスト表示するまでの時間")] private float[] texttime;
     [SerializeField,Header("移動時間")] private float movetime;
     // Start is called before the first frame update
     void Start()
@@ -18,9 +19,9 @@ public class TextAnimation : MonoBehaviour
         //_text[0].DOText(score[0].ToString(),5);
         for (int i = 0; i < _text.Length; i++)
         {
-            _text[i].DOText(score[i].ToString(), texttime).SetEase(Ease.Linear);
-            _text[i].transform.DOLocalMove(posion[i],movetime);
-            Debug.Log("a");
+            _text[i].DOText(score[i].ToString(), texttime[i]);
+            //_text[i].transform.DOLocalMove(posion[i],movetime);
+            //Debug.Log("a");
         }
     }
 }
--- Assets/Scr/画面表示/Result/ResultScoreDisplay.cs
+++ Assets/Scr/画面表示/Result/ResultScoreDisplay.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ResultScoreDisplay : MonoBehaviour
 {
     [SerializeField]
     private Text[] score;
-  
+    [SerializeField, Header("目標地点")]
+    private Vector2[] postion;
+    [SerializeField, Header("テキスト表示するまでの時間")] 
+    private float[] texttime;
     public void Set(string s)
     {
         for(int i= 0; i < score.Length; i++)
         {
-            score[i].text = s[i].ToString();
+            //score[i].text = s[i].ToString();
+            score[i].DOText(s[i].ToString(), texttime[i]).SetEase(Ease.Linear);
+            //score[i].transform.DOLocalMove(postion[i], 1.5f);
         }
     }
 }
