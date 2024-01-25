ae7649523ad1e5890860a37458546e4a07a949bf
syuugo <21cge0002is@edu.sca.ac.jp>
2024-01-25 16:11:18 +0900

    テキストアニメーション完成（多分）

--- Assets/Scr/伊藤/TextAnimation.cs
+++ Assets/Scr/伊藤/TextAnimation.cs
@@ -7,11 +7,20 @@ using DG.Tweening;
 public class TextAnimation : MonoBehaviour
 {
     //[SerializeField] private TMP_Text tmptext;
-    [SerializeField] private Text _text;
+    [SerializeField] private Text[] _text;
+    [SerializeField] private float[] score;
+    [SerializeField,Header("目標地点")] private Vector2[] posion; 
+    [SerializeField,Header("テキスト表示するまでの時間")] private float texttime;
+    [SerializeField,Header("移動時間")] private float movetime;
     // Start is called before the first frame update
     void Start()
     {
-        _text.transform.DOLocalMove(new Vector3(-150f,0f,0f),2f);
-        _text.DOText("1234567",2).SetEase(Ease.Linear);
+        //_text[0].DOText(score[0].ToString(),5);
+        for (int i = 0; i < _text.Length; i++)
+        {
+            _text[i].DOText(score[i].ToString(), texttime).SetEase(Ease.Linear);
+            _text[i].transform.DOLocalMove(posion[i],movetime);
+            Debug.Log("a");
+        }
     }
 }
