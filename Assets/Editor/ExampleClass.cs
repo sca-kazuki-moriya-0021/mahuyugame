8cd08411f124b8f8e8b7768f6054bfe123af76e0
moriya <21cge0021mk@edu.sca.ac.jp>
2024-01-29 14:50:41 +0900

    エディア拡張

---
+++ Assets/Editor/ExampleClass.cs
@@ -0,0 +1,42 @@
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public static class ExampleClass
+{
+    private const int WIDTH = 16;
+
+    private static readonly MethodInfo mGetIconForObject = typeof(EditorGUIUtility)
+        .GetMethod("GetIconForObject", BindingFlags.NonPublic | BindingFlags.Static);
+
+    [InitializeOnLoadMethod]
+    private static void Example()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += OnGUI;
+    }
+
+    private static void OnGUI(int instanceID, Rect selectionRect)
+    {
+        var go = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+        if (go == null)
+        {
+            return;
+        }
+
+        var parameters = new object[] { go };
+        var icon = mGetIconForObject.Invoke(null, parameters) as Texture;
+
+        if (icon == null)
+        {
+            return;
+        }
+
+        var pos = selectionRect;
+        pos.x = pos.xMax - WIDTH;
+        pos.width = WIDTH;
+
+        GUI.DrawTexture(pos, icon, ScaleMode.ScaleToFit, true);
+    }
+}
+
