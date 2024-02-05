c2711dce8141f35981af5466b6fd84513c5f51c7
moriya <21cge0021mk@edu.sca.ac.jp>
2024-02-05 13:27:47 +0900

    修正

--- Assets/Scr/シーン移動/Title/TitleSettingCon.cs
+++ Assets/Scr/シーン移動/Title/TitleSettingCon.cs
@@ -18,13 +18,12 @@ public class TitleSettingCon : MonoBehaviour
     [SerializeField]
     private EventSystem ev = EventSystem.current;
 
-    [SerializeField]
     private TotalGM totalGM;
 
     // Start is called before the first frame update
     void Start()
     {
-
+       totalGM = FindObjectOfType<TotalGM>();
     }
 
     // Update is called once per frame
@@ -50,12 +49,10 @@ public class TitleSettingCon : MonoBehaviour
     {
         if(toggle[0].isOn == true)
         {
-            Debug.Log("“ü‚Á‚½‚æ");
             totalGM.CutinWhetherFlag = true;
         }
         if(toggle[0].isOn == false)
         {
-            Debug.Log("ŠO‚µ‚½‚æ");
             totalGM.CutinWhetherFlag = false;
             Debug.Log(totalGM.CutinWhetherFlag);
         }
@@ -75,6 +72,7 @@ public class TitleSettingCon : MonoBehaviour
        anima.SetBool("Open", false);
     }
 
+
     public void OnAnimationCompleted()
     {
         for(int i = 0; i< toggle.Length; i++)
@@ -82,6 +80,17 @@ public class TitleSettingCon : MonoBehaviour
             toggle[i].gameObject.SetActive(true);
             toggle[i].interactable = true;
         }
+
+        if (totalGM.CutinWhetherFlag == true)
+            toggle[0].isOn = true;
+        else 
+            toggle[0].isOn = false;
+        
+        if (totalGM.LightBlinkingFlag == true)
+            toggle[1].isOn = true;
+        else
+            toggle[1].isOn = false;
+
         button.gameObject.SetActive(true);
         button.interactable = true;
     }
